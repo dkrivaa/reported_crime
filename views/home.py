71bc6a71b0bb56d8f6a538ff20c11670cdd531e9
dkrivaa <dannykrivaa@hotmail.com>
2024-12-07 10:19:32 +0200

    initial commit

--- views/home.py
+++ views/home.py
@@ -1,11 +1,13 @@
 import streamlit as st
+import time
 
 from functions.data import startup
 
 st.title('Reported Crime')
 
 with st.spinner('Loading Data......'):
-    startup()
+    # startup()
+    time.sleep(5)
+st.write('hello')
 
-st.write(st.session_state)
 
