ed1b64a3a312752b667ae8bf40463317e59de63f
dkrivaa <dannykrivaa@hotmail.com>
2024-12-07 10:23:04 +0200

    initial commit

--- views/home.py
+++ views/home.py
@@ -7,6 +7,7 @@ st.title('Reported Crime')
 
 with st.spinner('Loading Data......'):
     startup()
-st.write('hello')
 
+for key in st.session_state.keys():
+    st.write(f"- {key}: {st.session_state[key]}")
 
