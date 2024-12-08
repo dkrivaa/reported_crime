98e340a0f34f088e758ec8b2757e74ef89e84dc0
dkrivaa <dannykrivaa@hotmail.com>
2024-12-08 10:12:07 +0200

    initial commit

--- views/home.py
+++ views/home.py
@@ -12,14 +12,8 @@ name = [f'{data[i]['Year'].unique().tolist()[0]}_{data[i]['Quarter'].unique().to
         for i in range(len(data))]
 df = pd.DataFrame(quarter_crime, name)
 
-# st.bar_chart(df)
+st.bar_chart(df)
 
-chart = alt.Chart(df).mark_bar().encode(
-
-    y=alt.Y('quarter_crime', scale=alt.Scale(domain=[75000, 105000]))
-)
-# Display the chart in Streamlit
-st.altair_chart(chart)
 st.write(df)
 
 
