999cfb813775be7615565e8aa9496a190f43926f
dkrivaa <dannykrivaa@hotmail.com>
2024-12-08 14:35:52 +0200

    initial commit

--- functions/form_elements.py
+++ functions/form_elements.py
@@ -1,19 +1,22 @@
 import streamlit as st
 import pandas as pd
 
+
 def district_element():
     return st.selectbox('Police District', options=st.session_state['district_dict'].values(), index=None)
 
 
-def merhav_element(district=None):
-
-    if district is None:
+def merhav_element(districtKod=None):
+    if districtKod is None:
         return st.selectbox('Police Area', options=st.session_state['merhav_dict'].values(), index=None)
-    elif district is not None:
-        merhavim = st.session_state['district_merhav_dict'][district]
+    elif districtKod is not None:
+        merhavim = st.session_state['district_merhav_dict'][districtKod]
         options = [st.session_state['merhav_dict'][merhav] for merhav in merhavim]
         return st.selectbox('Police Area', options=options, index=None)
 
+
+def station_element(merhavKod=None):
+    pass
 
 
 
