352e81c27cdaed722181f470491cdd7a500dffaa
dkrivaa <dannykrivaa@hotmail.com>
2024-12-08 09:48:13 +0200

    initial commit

--- views/home.py
+++ views/home.py
@@ -8,7 +8,9 @@ st.title('Reported Crime')
 data = startup()
 
 quarter_crime = [len(data[i]) for i in range(len(data))]
+name = [data[i]['Quarter'].unique().tolist()[0] for i in range(len(data))]
 st.write(quarter_crime)
+st.write(name)
 
 st.bar_chart(quarter_crime)
 
