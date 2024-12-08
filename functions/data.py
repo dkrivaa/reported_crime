884a4508ccfc9b702926aa25da0929214c81c777
dkrivaa <dannykrivaa@hotmail.com>
2024-12-08 07:23:28 +0200

    initial commit

--- functions/data.py
+++ functions/data.py
@@ -8,10 +8,10 @@ def resources():
     return {
         2019: '3b3cf0d8-67d9-4719-9dfa-f73ab7ab9f68',
         2020: '520597e3-6003-4247-9634-0ae85434b971',
-        # 2021: '3f71fd16-25b8-4cfe-8661-e6199db3eb12',
-        # 2022: 'a59f3e9e-a7fe-4375-97d0-76cea68382c1',
-        # 2023: '32aacfc9-3524-4fba-a282-3af052380244',
-        # 2024: '5fc13c50-b6f3-4712-b831-a75e0f91a17e',
+        2021: '3f71fd16-25b8-4cfe-8661-e6199db3eb12',
+        2022: 'a59f3e9e-a7fe-4375-97d0-76cea68382c1',
+        2023: '32aacfc9-3524-4fba-a282-3af052380244',
+        2024: '5fc13c50-b6f3-4712-b831-a75e0f91a17e',
     }
 
 
@@ -63,10 +63,60 @@ def get_data(resource_id, filters=None):
 
 @st.cache_data(show_spinner='Loading data.......', persist='disk')
 def startup():
-    return [(next((k for k, v in resources().items() if v == resource), None), quarter,
-             pd.DataFrame(get_data(resource, filters={'Quarter': quarter, })))
-            for resource in resources().values()
-            for quarter in quarters()]
+    yeshuv_set = set()
+    district_set = set()
+    merhav_set = set()
+    station_set = set()
+    crimeGroup_set = set()
+    crimeType_set = set()
+
+    df_list = []
+
+    for resource in resources().values():
+        for quarter in quarters():
+            df = pd.DataFrame(get_data(resource, filters={'Quarter': quarter, }))
+
+            # Copying regional councils to municipal and multiplying code to avoid identical codes
+            df['YeshuvKod'] = df['YeshuvKod'].fillna((df['municipalKod'] * 10000).where(df['municipalKod'].notna()))
+            df['Yeshuv'] = df['Yeshuv'].fillna(df['municipalName'].where(df['municipalKod'].notna()))
+
+            df = df.dropna(subset=['YeshuvKod'])
+
+            def make_pairs(codes, names):
+                return set(zip(df[codes], df[names]))
+
+            yeshuv_set.update(make_pairs('YeshuvKod', 'Yeshuv'))
+            district_set.update(make_pairs('PoliceDistrictKod	', 'PoliceDistrict'))
+            merhav_set.update(make_pairs('PoliceMerhavKod', 'PoliceMerhav'))
+            station_set.update(make_pairs('PoliceStationKod', 'PoliceStation'))
+            crimeGroup_set.update(make_pairs('StatisticGroupKod', 'StatisticGroup'))
+            crimeType_set.update(make_pairs('StatisticTypeKod', 'StatisticType	'))
+
+            columns_to_drop = ['FictiveIDNumber', 'Yeshuv', 'PoliceDistrict', 'PoliceMerhav',
+                               'PoliceStation', 'municipalKod', 'municipalName', 'StatisticAreaKod',
+                               'StatisticArea', 'StatisticGroup', 'StatisticType']
+            df.drop(columns=columns_to_drop, inplace=True)
+
+            df_list.append(df)
+
+
+    def make_dict(name, pair_set):
+        if name not in st.session_state:
+            st.session_state[name] = dict(pair_set)
+
+    make_dict('yeshuv_dict', yeshuv_set)
+    make_dict('district_dict', district_set)
+    make_dict('merhav_dict', merhav_set)
+    make_dict('station_dict', station_set)
+    make_dict('crimeGroup_dict', crimeGroup_set)
+    make_dict('crimeType_set', crimeType_set)
+
+    return df_list
+
+
+
+
+
 
 
 
