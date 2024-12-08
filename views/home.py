2d85596f01a1563518c9d91d3c50390471cf92b4
dkrivaa <dannykrivaa@hotmail.com>
2024-12-08 14:32:08 +0200

    initial commit

--- views/home.py
+++ views/home.py
@@ -15,22 +15,21 @@ with st.expander('Select data'):
     district = district_element()
     if district is not None:
         districtKod = [k for k, v in st.session_state['district_dict'].items() if v == district][0]
-        st.write(districtKod)
 
         merhav = merhav_element(districtKod)
+        if merhav is not None:
+            merhavKod = [k for k, v in st.session_state['merhav_dict'].items() if v == merhav][0]
 
-        quarter_crime = [data[i].loc[data[i]['PoliceDistrictKod'] == districtKod].shape[0] for i in range(len(data))]
-        st.write(quarter_crime)
-        name = [f'{data[i]['Year'].unique().tolist()[0]}_{data[i]['Quarter'].unique().tolist()[0]}'
-                for i in range(len(data))]
-        df = pd.DataFrame(quarter_crime, name, columns=['Data'])
+quarter_crime = [data[i].loc[data[i]['PoliceDistrictKod'] == districtKod].shape[0] for i in range(len(data))]
+name = [f'{data[i]['Year'].unique().tolist()[0]}_{data[i]['Quarter'].unique().tolist()[0]}'
+        for i in range(len(data))]
+df = pd.DataFrame(quarter_crime, name, columns=['Data'])
 
-        st.bar_chart(df)
+st.bar_chart(df)
 
-        if st.button('Chart Data'):
-            st.write('')
-            st.write(df)
+if st.button('Chart Data'):
+    st.write('')
+    st.write(df)
 
-st.write(st.session_state['merhav_station_dict'])
 
 
