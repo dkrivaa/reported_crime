c46b874add33e715c04b2f77b4486df2c4d359c4
dkrivaa <dannykrivaa@hotmail.com>
2024-12-08 10:48:14 +0200

    initial commit

--- views/home.py
+++ views/home.py
@@ -10,17 +10,20 @@ st.title('Reported Crime')
 st.divider()
 
 with st.expander('Select data'):
-    district = st.selectbox('District', options=st.session_state['district_dict'].values())
+    district = st.selectbox('District', options=st.session_state['district_dict'].values(), index=None)
+    if district is not None:
+        district = (k for k,v in st.session_state['district_dict'].items() if v == district)
 
-quarter_crime = [len(data[i]) for i in range(len(data))]
-name = [f'{data[i]['Year'].unique().tolist()[0]}_{data[i]['Quarter'].unique().tolist()[0]}'
-        for i in range(len(data))]
-df = pd.DataFrame(quarter_crime, name, columns=['Data'])
+if district is not None:
+        quarter_crime = [len(data[i].loc[data[i]['PoliceDistrictKod']]) for i in range(len(data))]
+        name = [f'{data[i]['Year'].unique().tolist()[0]}_{data[i]['Quarter'].unique().tolist()[0]}'
+                for i in range(len(data))]
+        df = pd.DataFrame(quarter_crime, name, columns=['Data'])
 
-st.bar_chart(df)
+        st.bar_chart(df)
 
-if st.button('Chart Data'):
-        st.write('')
-        st.write(df)
+        if st.button('Chart Data'):
+                st.write('')
+                st.write(df)
 
 
