5761df87f1171bb728f4d5622c4545d58cfa82e5
dkrivaa <dannykrivaa@hotmail.com>
2024-12-08 10:53:02 +0200

    initial commit

--- views/home.py
+++ views/home.py
@@ -12,11 +12,11 @@ st.divider()
 with st.expander('Select data'):
     district = st.selectbox('District', options=st.session_state['district_dict'].values(), index=None)
     if district is not None:
-        district = (k for k,v in st.session_state['district_dict'].items() if v == district)
-st.write(district)
+        districtKod = (k for k, v in st.session_state['district_dict'].items() if v == district)
+st.write(districtKod)
 
-if district is not None:
-        quarter_crime = [len(data[i].loc[data[i]['PoliceDistrictKod'] == district]) for i in range(len(data))]
+if districtKod is not None:
+        quarter_crime = [len(data[i].loc[data[i]['PoliceDistrictKod'] == districtKod]) for i in range(len(data))]
         name = [f'{data[i]['Year'].unique().tolist()[0]}_{data[i]['Quarter'].unique().tolist()[0]}'
                 for i in range(len(data))]
         df = pd.DataFrame(quarter_crime, name, columns=['Data'])
