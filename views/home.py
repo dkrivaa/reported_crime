ee616de52bc3927e375f83b3f520393de3de04e8
dkrivaa <dannykrivaa@hotmail.com>
2024-12-08 07:44:34 +0200

    initial commit

--- views/home.py
+++ views/home.py
@@ -7,11 +7,7 @@ st.title('Reported Crime')
 
 data = startup()
 
-period = [f'{df[i]['Year']}_{df[i]['Quarter']}' for df in data for i in range(len(df))]
-# chart_data = [len(item[2]) for item in data]
-
-st.write(period)
-# st.write(chart_data)
+st.write(len(data))
 
 
 
