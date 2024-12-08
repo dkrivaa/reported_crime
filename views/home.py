79d7c8eb7839d255a9c4a7c69ff43bb288798f6b
dkrivaa <dannykrivaa@hotmail.com>
2024-12-08 09:56:23 +0200

    initial commit

--- views/home.py
+++ views/home.py
@@ -11,7 +11,7 @@ data = startup()
 quarter_crime = [len(data[i]) for i in range(len(data))]
 name = [f'{data[i]['Year'].unique().tolist()[0]}_{data[i]['Quarter'].unique().tolist()[0]}'
         for i in range(len(data))]
-df = pd.DataFrame(name, quarter_crime)
+df = pd.DataFrame(quarter_crime, name)
 
 st.bar_chart(df)
 st.write(df)
