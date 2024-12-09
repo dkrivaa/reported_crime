51c955b17978ebcf447e7a7b45180a8e3fcb92a4
dkrivaa <dannykrivaa@hotmail.com>
2024-12-09 15:59:51 +0200

    initial commit

---
+++ functions/filters.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pandas as pd
+
+
+# Filter functions
+def filter_by_yeshuv(df, yeshuvKod=None):
+    if yeshuvKod is not None:
+        return df[df['YeshuvKod'] == yeshuvKod]
+    else:
+        return
+
+
+def filter_by_district(df, districtKod=None):
+    if districtKod is not None:
+        return df[df['PoliceDistrictKod'] == districtKod]
+    else:
+        return
+
+
+def filter_by_merhav(df, merhavKod=None):
+    if merhavKod is not None:
+        return df[df['PoliceMerhavKod'] == merhavKod]
+    else:
+        return
+
+
+def filter_by_station(df, stationKod=None):
+    if stationKod is not None:
+        return df[df['PoliceStationKod'] == stationKod]
+    else:
+        return
+
+
+def filter_by_crimeGroup(df, crimeGroupKod=None):
+    if crimeGroupKod is not None:
+        return df[df['StatisticGroupKod'] == crimeGroupKod]
+    else:
+        return
+
+
+def filter_by_crimeType(df, crimeTypeKod=None):
+    if crimeTypeKod is not None:
+        return df[df['StatisticTypeKod'] == crimeTypeKod]
+    else:
+        return
+
+
+def selection_filter(df, k, v=None):
+    if v is not None:
+        return df[df[k] == v]
+    else:
+        return
+
+
+def use_filters(df, filter_dict):
+    filtered_df = df
+    for k, v in filter_dict.items():
+        filtered_df = selection_filter(df, k, v)
+    return filtered_df.shape[0]
--- views/home.py
+++ views/home.py
@@ -5,6 +5,7 @@ import altair as alt
 from functions.data import startup
 from functions.form_elements import (geography, yeshuv_element, district_element, merhav_element, station_element,
                                      crimeGroup_element, crimeType_element)
+from functions.filters import use_filters
 
 st.title('Reported Crime')
 st.divider()
@@ -18,6 +19,15 @@ data = startup()
 
 quarter_crime = [data[i].shape[0] for i in range(len(data))]
 
+filter_dict = {
+    'yeshuvKod': None,
+    'PoliceDistrictKod': None,
+    'PoliceMerhavKod': None,
+    'PoliceStationKod': None,
+    'StatisticGroupKod': None,
+    'StatisticTypeKod': None
+}
+
 with st.expander('Select Geographic Area'):
     # Radio
     geo = geography()
@@ -26,58 +36,68 @@ with st.expander('Select Geographic Area'):
         yeshuv = yeshuv_element()
         if yeshuv is not None:
             yeshuvKod = [k for k, v in st.session_state['yeshuv_dict'].items() if v == yeshuv][0]
+            filter_dict['yeshuvKod'] = yeshuvKod
 
-            quarter_crime = [data[i][data[i]['YeshuvKod'] == yeshuvKod].shape[0] for i in range(len(data))]
+            # quarter_crime = [data[i][data[i]['YeshuvKod'] == yeshuvKod].shape[0] for i in range(len(data))]
 
     elif geo == 1:
         district = district_element()
         if district is not None:
             districtKod = [k for k, v in st.session_state['district_dict'].items() if v == district][0]
+            filter_dict['PoliceStationKod'] = districtKod
 
-            quarter_crime = [data[i][data[i]['PoliceDistrictKod'] == districtKod].shape[0] for i in
-                             range(len(data))]
+            # quarter_crime = [data[i][data[i]['PoliceDistrictKod'] == districtKod].shape[0] for i in
+            #                  range(len(data))]
 
             merhav = merhav_element(districtKod)
             if merhav is not None:
                 merhavKod = [k for k, v in st.session_state['merhav_dict'].items() if v == merhav][0]
+                filter_dict['PoliceMerhavKod'] = merhavKod
 
-                quarter_crime = [data[i][data[i]['PoliceMerhavKod'] == merhavKod].shape[0] for i in
-                                 range(len(data))]
+                # quarter_crime = [data[i][data[i]['PoliceMerhavKod'] == merhavKod].shape[0] for i in
+                #                  range(len(data))]
 
                 station = station_element(merhavKod)
                 if station is not None:
                     stationKod = [k for k, v in st.session_state['station_dict'].items() if v == station][0]
+                    filter_dict['PoliceStationKod'] = stationKod
 
-                    quarter_crime = [data[i][data[i]['PoliceStationKod'] == stationKod].shape[0] for i in
-                                     range(len(data))]
+                    # quarter_crime = [data[i][data[i]['PoliceStationKod'] == stationKod].shape[0] for i in
+                    #                  range(len(data))]
 
 with st.expander('Select Crime'):
     crimeGroup = crimeGroup_element()
     if crimeGroup is not None:
         crimeGroupKod = [k for k, v in st.session_state['crimeGroup_dict'].items() if v == crimeGroup][0]
+        filter_dict['StatisticGroupKod'] = crimeGroupKod
 
-        quarter_crime = [data[i][data[i]['StatisticGroupKod'] == crimeGroupKod].shape[0] for i in
-                         range(len(data))]
+        # quarter_crime = [data[i][data[i]['StatisticGroupKod'] == crimeGroupKod].shape[0] for i in
+        #                  range(len(data))]
 
         crimeType = crimeType_element(crimeGroupKod)
         if crimeType is not None:
             crimeTypeKod = [k for k, v in st.session_state['crimeType_dict'].items() if v == crimeType][0]
+            filter_dict['StatisticTypeKod'] = crimeTypeKod
 
-            quarter_crime = [data[i][data[i]['StatisticTypeKod'] == crimeTypeKod].shape[0] for i in
-                             range(len(data))]
+            # quarter_crime = [data[i][data[i]['StatisticTypeKod'] == crimeTypeKod].shape[0] for i in
+            #                  range(len(data))]
+
+
+quarter_crime = [use_filters(df, filter_dict) for df in data]
+
+st.write(quarter_crime)
 
 
 
-
-name = [f'{data[i]['Year'].unique().tolist()[0]}_{data[i]['Quarter'].unique().tolist()[0]}'
-        for i in range(len(data))]
-df = pd.DataFrame(quarter_crime, name, columns=['Data'])
-
-st.bar_chart(df)
-
-if st.button('Chart Data'):
-    st.write('')
-    st.write(df)
+# name = [f'{data[i]['Year'].unique().tolist()[0]}_{data[i]['Quarter'].unique().tolist()[0]}'
+#         for i in range(len(data))]
+# df = pd.DataFrame(quarter_crime, name, columns=['Data'])
+#
+# st.bar_chart(df)
+#
+# if st.button('Chart Data'):
+#     st.write('')
+#     st.write(df)
 
 
 
